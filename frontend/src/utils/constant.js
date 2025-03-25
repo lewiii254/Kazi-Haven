f745ef89f3c37660896c208e7ab6a0465ea23c14
MARKLEWIS MUTUGI <164312183+lewiii254@users.noreply.github.com>
2025-03-25 23:45:49 +0400

    backend Url updated

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,9 +1,9 @@
-export const USER_BASE_URL = "http://localhost:5000/api/user";
-export const JOB_BASE_URL = "http://localhost:5000/api/job";
+export const USER_BASE_URL = "https://kazi-haven-2.onrender.com/api/user";
+export const JOB_BASE_URL = "https://kazi-haven-2.onrender.com/api/job";
 export const APPLICATION_BASE_URL =
-  "http://localhost:5000/api/application";
+  "https://kazi-haven-2.onrender.com/api/application";
 export const COMPANY_BASE_URL =
-  "http://localhost:5000/api/company";
+  "https://kazi-haven-2.onrender.com/api/company";
 
 export const getInitialName = (userName) => {
   if (!userName) return "CN";
